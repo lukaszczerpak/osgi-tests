97dbc55e0affaf0c9b544b0175e7ea4e5edb2add
ancoron <ancoron.luciferis@gmail.com>
2011-02-19 01:03:33 +0100

    initial implementation

--- test-support/base/src/main/java/org/ancoron/osgi/test/OSGiTestSupport.java
+++ test-support/base/src/main/java/org/ancoron/osgi/test/OSGiTestSupport.java
@@ -16,15 +16,46 @@
 
 package org.ancoron.osgi.test;
 
+import static org.testng.Assert.*;
+
 import java.util.Map;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.launch.Framework;
+import org.testng.annotations.Test;
 
 /**
  *
  * @author ancoron
  */
-public abstract class OSGiTestSupport {
+public abstract class OSGiTestSupport<T extends Framework> {
     
-    public abstract void startFramework(final Map<String, String> properties);
+    private T framework;
+    
+    public T getFramework() {
+        return framework;
+    }
+    
+    protected void setFramework(T framework) {
+        this.framework = framework;
+    }
 
-    public abstract void stopFramework();
+    public abstract void configureFramework(final Map<String, String> properties);
+    
+    @Test(alwaysRun=true)
+    public void startFramework() {
+        try {
+            getFramework().start();
+        } catch (BundleException ex) {
+            fail("Unable to start framework", ex);
+        }
+    }
+
+    @Test(alwaysRun=true)
+    public void stopFramework() {
+        try {
+            getFramework().stop();
+        } catch (BundleException ex) {
+            fail("Unable to stop framework", ex);
+        }
+    }
 }
