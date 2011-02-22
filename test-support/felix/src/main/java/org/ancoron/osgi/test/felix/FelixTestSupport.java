cfbeaca7b4ce99b4cee928fe5ec5856543615d75
ancoron <ancoron.luciferis@gmail.com>
2011-02-22 01:00:38 +0100

    Support custom framework configuration

--- test-support/felix/src/main/java/org/ancoron/osgi/test/felix/FelixTestSupport.java
+++ test-support/felix/src/main/java/org/ancoron/osgi/test/felix/FelixTestSupport.java
@@ -17,11 +17,18 @@
 package org.ancoron.osgi.test.felix;
 
 import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.ancoron.osgi.test.OSGiTestSupport;
 import org.apache.felix.framework.Felix;
 import org.apache.felix.framework.cache.BundleCache;
 import org.apache.felix.framework.util.StringMap;
+import org.apache.felix.framework.util.Util;
 
 import static org.testng.Assert.*;
 
@@ -30,6 +37,8 @@ import static org.testng.Assert.*;
  * @author ancoron
  */
 public class FelixTestSupport extends OSGiTestSupport<Felix> {
+    
+    private final Pattern UNRESOLVED = Pattern.compile(".*$\\{(.+)\\}.*");
 
     /**
      * A simple  method to customize the configuration for Felix to meet your
@@ -40,6 +49,50 @@ public class FelixTestSupport extends OSGiTestSupport<Felix> {
      * @param configMap A StringMap with very basic configuration
      */
     public void customizeFrameworkConfig(final Map configMap) {
+        File config = new File("src/main/resources/felix/conf/config.properties");
+        
+        if(config.exists()) {
+            try {
+                System.out.println("Loading Felix configuration from " + config.getCanonicalPath());
+                URL configUrl = config.toURI().toURL();
+
+                Properties props = new Properties();
+                InputStream is = null;
+                try {
+                    // Try to load config.properties.
+                    is = configUrl.openConnection().getInputStream();
+                    props.load(is);
+                    is.close();
+                } catch(Exception ex) {
+                    // Try to close input stream if we have one.
+                    try {
+                        if(is != null) is.close();
+                    } catch (Exception ex2) {}
+                }
+
+                // Perform variable substitution for system properties.
+                for(Enumeration e = props.propertyNames(); e.hasMoreElements(); ) {
+                    String name = (String) e.nextElement();
+                    props.setProperty(name, Util.substVars(props.getProperty(name), name, null, props));
+                }
+                
+                for(Enumeration e = props.propertyNames(); e.hasMoreElements(); ) {
+                    String name = (String) e.nextElement();
+                    String value = (String) props.getProperty(name);
+                    Matcher m = UNRESOLVED.matcher(value);
+                    if(m.find()) {
+                        fail("Felix configuration property '" + m.group(1) + "' not resolved");
+                    }
+                    configMap.put(name, value);
+                }
+                
+                System.out.println("Configured " + configMap.size() + " properties");
+            } catch(Exception x) {
+                fail("Unable to load Felix configuration", x);
+            }
+        } else {
+            System.out.println("No Felix configuration available at " + config.getAbsolutePath());
+        }
     }
 
     @Override
