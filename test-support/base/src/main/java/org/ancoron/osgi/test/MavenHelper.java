e308e4414e7df0b85304f3c8c27aa9824cbbfec2
ancoron <ancoron.luciferis@gmail.com>
2011-03-05 16:58:50 +0100

    Add artifact lookup in Maven repository

--- test-support/base/src/main/java/org/ancoron/osgi/test/MavenHelper.java
+++ test-support/base/src/main/java/org/ancoron/osgi/test/MavenHelper.java
@@ -16,7 +16,9 @@
 package org.ancoron.osgi.test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import javax.xml.stream.XMLStreamException;
 
 /**
  *
@@ -29,12 +31,59 @@ public class MavenHelper
     static {
         mavenRepo = System.getProperty("maven.repo");
     }
+    
+    private static POMReader pom = null;
+    
+    private static POMReader read() throws XMLStreamException, FileNotFoundException {
+        if(pom == null) {
+            pom = new POMReader("pom.xml", mavenRepo);
+            pom.read();
+        }
+        
+        return pom;
+    }
 
     public static List<File> getProvidedArtifacts() throws Exception
     {
-        POMReader reader = new POMReader("pom.xml", mavenRepo);
-        reader.read();
+        return read().getProvided();
+    }
+    
+    public static File getArtifact(String id) {
+        String[] ids = id.split(":");
         
-        return reader.getProvided();
+        String[] props = new String[4];
+        
+        // groupId...
+        props[0] = ids[0];
+
+        // artifactId...
+        props[1] = ids[1];
+        
+        // version...
+        if(ids.length > 2) {
+            props[2] = ids[2];
+        } else {
+            props[2] = pom.getVersion();
+        }
+        
+        // type...
+        if(ids.length > 3) {
+            props[3] = ids[3];
+        } else {
+            props[3] = "jar";
+        }
+        
+        return getArtifact(props[0], props[1], props[2], props[3]);
+    }
+
+    public static File getArtifact(String groupId, String artifactId, String version, String type) {
+        if(type == null) {
+            type = "jar";
+        }
+        
+        String path = groupId.replaceAll("\\.", "/") + "/" + artifactId + "/"
+                + version + "/" + artifactId + "-" + version + "." + type;
+        
+        return new File(mavenRepo, path);
     }
 }
