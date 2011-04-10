7277aab6b8386c69a9ecedea5ca487630ccd710d
ancoron <ancoron.luciferis@gmail.com>
2011-04-10 20:26:47 +0200

    Add first test WAB + implement first web tests using Apache HTTP-Components

--- test/glassfish-derby/src/test/java/org/ancoron/osgi/test/glassfish/GlassfishDerbyTest.java
+++ test/glassfish-derby/src/test/java/org/ancoron/osgi/test/glassfish/GlassfishDerbyTest.java
@@ -16,6 +16,13 @@
 
 package org.ancoron.osgi.test.glassfish;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -23,7 +30,12 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import org.ancoron.movie.MovieService;
 import org.ancoron.movie.MovieServiceException;
 import org.ancoron.movie.jpa.MovieJPAService;
@@ -31,6 +43,27 @@ import org.ancoron.movie.persistence.MoviePersistenceException;
 import org.ancoron.movie.persistence.MoviePersistenceService;
 import org.ancoron.movie.test.MovieServiceTest;
 import org.ancoron.osgi.test.ejb.slsb.SLSBInterface;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
@@ -131,7 +164,7 @@ public class GlassfishDerbyTest extends GlassfishDerbyTestSupport {
         }
     }
     
-    @Test(timeOut=10000, dependsOnMethods={"testMovieService"})
+    @Test(timeOut=30000, dependsOnMethods={"testMovieService"})
     public void testUpdatePersistenceAPI() {
         logTest();
         
@@ -158,6 +191,128 @@ public class GlassfishDerbyTest extends GlassfishDerbyTestSupport {
         testMovieService();
     }
     
+    protected DefaultHttpClient getHTTPClient() throws NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sslContext = SSLContext.getInstance("SSL");
+
+        // set up a TrustManager that trusts everything
+        sslContext.init(null, new TrustManager[] { new X509TrustManager() {
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                System.out.println("getAcceptedIssuers =============");
+                return null;
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                System.out.println("checkClientTrusted =============");
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                System.out.println("checkServerTrusted =============");
+            }
+        } }, new SecureRandom());
+        
+        SSLSocketFactory sf = new SSLSocketFactory(sslContext,
+                SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+        Scheme httpsScheme = new Scheme("https", 8181, sf);
+
+        PlainSocketFactory plain = new PlainSocketFactory();
+        Scheme httpScheme = new Scheme("http", 8080, plain);
+        SchemeRegistry schemeRegistry = new SchemeRegistry();
+        schemeRegistry.register(httpsScheme);
+        schemeRegistry.register(httpScheme);
+
+        HttpParams params = new BasicHttpParams();
+        
+        ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(schemeRegistry);
+        // Increase max total connection to 200
+        cm.setMaxTotal(200);
+        // Increase default max connection per route to 20
+        cm.setDefaultMaxPerRoute(20);
+
+        DefaultHttpClient httpClient = new DefaultHttpClient(cm, params);
+        httpClient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION,
+                HttpVersion.HTTP_1_1);
+        httpClient.getParams().setParameter(CoreProtocolPNames.HTTP_CONTENT_CHARSET,
+                "UTF-8");
+        return httpClient;
+    }
+    
+    @Test(dependsOnGroups={"glassfish-osgi-startup"})
+    public void testWebAvailability() throws NoSuchAlgorithmException, KeyManagementException, IOException {
+        logTest();
+        
+        DefaultHttpClient http = getHTTPClient();
+        try {
+            HttpGet httpget = new HttpGet("http://[::1]/movie/dummy/");
+            httpget.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, 
+                HttpVersion.HTTP_1_1); // Use HTTP 1.1 for this request only
+            httpget.getParams().setParameter(CoreProtocolPNames.USE_EXPECT_CONTINUE, 
+                Boolean.FALSE);
+
+            HttpResponse res = http.execute(httpget);
+            
+            if(res.getStatusLine().getStatusCode() == 200) {
+                fail("Gained access to secured page at '" + httpget.getURI().toASCIIString()
+                        + "' [status=" + res.getStatusLine().getStatusCode() + "]");
+            }
+            
+            log.log(Level.INFO, "[HTTP] Got response for ''{0}'' [status={1}]",
+                    new Object[] {httpget.getURI().toASCIIString(), res.getStatusLine().getStatusCode()});
+
+            HttpGet httpsget = new HttpGet("https://[::1]/movie/dummy/");
+            httpsget.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, 
+                HttpVersion.HTTP_1_1); // Use HTTP 1.1 for this request only
+            httpsget.getParams().setParameter(CoreProtocolPNames.USE_EXPECT_CONTINUE, 
+                Boolean.FALSE);
+
+            res = http.execute(httpsget);
+            
+            if(res.getStatusLine().getStatusCode() == 200) {
+                fail("Gained access to secured page at '" + httpsget.getURI().toASCIIString()
+                        + "' [status=" + res.getStatusLine().getStatusCode() + "] "
+                        + res.getStatusLine().getReasonPhrase());
+            }
+            
+            log.log(Level.INFO, "[HTTP] Got response for ''{0}'' [status={1}]",
+                    new Object[] {httpget.getURI().toASCIIString(), res.getStatusLine().getStatusCode()});
+            
+            http.getCredentialsProvider().setCredentials(
+                    new AuthScope("[::1]", 8181),
+                    new UsernamePasswordCredentials("fail", "fail"));
+            
+            res = http.execute(httpsget);
+            
+            if(res.getStatusLine().getStatusCode() == 200) {
+                fail("Gained access with invalid user role to secured page at '"
+                        + httpsget.getURI().toASCIIString()
+                        + "' [status=" + res.getStatusLine().getStatusCode() + "] "
+                        + res.getStatusLine().getReasonPhrase());
+            }
+            
+            log.log(Level.INFO, "[HTTP] Got response for ''{0}'' [status={1}]",
+                    new Object[] {httpget.getURI().toASCIIString(), res.getStatusLine().getStatusCode()});
+            
+            http.getCredentialsProvider().setCredentials(
+                    new AuthScope("[::1]", 8181),
+                    new UsernamePasswordCredentials("test", "test"));
+            
+            res = http.execute(httpsget);
+            
+            if(res.getStatusLine().getStatusCode() != 200) {
+                fail("Failed to access " + httpsget.getURI().toASCIIString()
+                        + ": [status=" + res.getStatusLine().getStatusCode() + "] "
+                        + res.getStatusLine().getReasonPhrase());
+            }
+            
+            log.log(Level.INFO, "[HTTP] Got response for ''{0}'' [status={1}]",
+                    new Object[] {httpget.getURI().toASCIIString(), res.getStatusLine().getStatusCode()});
+        } finally {
+            http.getConnectionManager().shutdown();
+        }
+    }
+    
     @Override
     public void stopFramework() {
         BundleContext ctx = getFramework().getBundleContext();
@@ -179,8 +334,8 @@ public class GlassfishDerbyTest extends GlassfishDerbyTestSupport {
                 for(ServiceReference ref : refs) {
                     String[] clazz = (String[]) ref.getProperty(Constants.OBJECTCLASS);
                     Long id = (Long) ref.getProperty(Constants.SERVICE_ID);
-                    logLines.add(String.format("    |%11s| id=%d, interfaces: %s",
-                            "providing", id, Arrays.toString(clazz)));
+                    logLines.add(String.format("    |%11s| id=%4d, interfaces: %s",
+                            "-->", id, Arrays.toString(clazz)));
                 }
             }
 
@@ -189,8 +344,8 @@ public class GlassfishDerbyTest extends GlassfishDerbyTestSupport {
                 for(ServiceReference ref : refs) {
                     String[] clazz = (String[]) ref.getProperty(Constants.OBJECTCLASS);
                     Long id = (Long) ref.getProperty(Constants.SERVICE_ID);
-                    logLines.add(String.format("    |%11s| <-- id=%d, bundle=%d, interfaces: %s",
-                            "consuming", id, ref.getBundle().getBundleId(), Arrays.toString(clazz)));
+                    logLines.add(String.format("    |%11s| id=%4d, bundle=%4d, interfaces: %s",
+                            "<--", id, ref.getBundle().getBundleId(), Arrays.toString(clazz)));
                 }
             }
         }
