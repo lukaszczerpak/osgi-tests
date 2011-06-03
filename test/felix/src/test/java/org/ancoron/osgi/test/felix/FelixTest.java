9130b1b595c391f0d20325b1b23a3ef937a4e168
ancoron <ancoron.luciferis@gmail.com>
2011-06-04 01:03:12 +0200

    Extend Felix test cases

--- test/felix/src/test/java/org/ancoron/osgi/test/felix/FelixTest.java
+++ test/felix/src/test/java/org/ancoron/osgi/test/felix/FelixTest.java
@@ -16,10 +16,132 @@
 
 package org.ancoron.osgi.test.felix;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.ancoron.osgi.test.helloservice.HelloService;
+import org.osgi.framework.Bundle;
+import org.osgi.util.tracker.ServiceTracker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
  *
  * @author ancoron
  */
 public class FelixTest extends FelixTestSupport {
+    
+    @Test(timeOut=10000L)
+    public void uninstallSimpleBundle() {
+        logTest();
 
+        Bundle serviceBundle = null;
+        for(Bundle b : bundles) {
+            if("org.ancoron.osgi.test.bundles-simple".equals(b.getSymbolicName())) {
+                serviceBundle = b;
+                break;
+            }
+        }
+        
+        uninstallBundle(serviceBundle);
+    }
+    
+    @Test(timeOut=10000L)
+    public void installHelloService() {
+        logTest();
+
+        installAndStart("org.ancoron.osgi.test:hello-service");
+        
+        HelloService svc = waitForService(HelloService.class);
+        
+        if(svc == null) {
+            Assert.fail("Unable to get HelloService");
+        }
+    }
+    
+    @Test(timeOut=10000L, dependsOnMethods="installHelloService")
+    public void uninstallHelloService() {
+        logTest();
+
+        Bundle helloBundle = getBundlesBySymbolicName("org.ancoron.osgi.test.hello-service").iterator().next();
+        uninstallBundle(helloBundle);
+    }
+    
+    @Test(timeOut=10000L, dependsOnMethods="uninstallHelloService")
+    public void testWaitForService() throws InterruptedException, ExecutionException {
+        logTest();
+
+        Bundle hostBundle = null;
+        for(Bundle b : bundles) {
+            if("org.ancoron.osgi.test.fragments-simple-host".equals(b.getSymbolicName())) {
+                hostBundle = b;
+                break;
+            }
+        }
+        
+        final Bundle client = hostBundle;
+
+        Future<Object> svc = Executors.newSingleThreadExecutor().submit(new Callable<Object>() {
+
+            @Override
+            public Object call() throws Exception {
+                ServiceTracker st = new ServiceTracker(client.getBundleContext(),
+                        HelloService.class.getName(), null);
+                st.open();
+                try {
+                    return st.waitForService(10000L);
+                } finally {
+                    st.close();
+                }
+            }
+        });
+        
+        // installBundle(MavenHelper.getArtifact("org.ancoron.osgi.test", "hello-service", "1.0.1-SNAPSHOT", "jar"),
+        //         getFramework().getBundleContext());
+        
+        installAndStart("org.ancoron.osgi.test:hello-service");
+        
+        svc.get();
+    }
+    
+    @Test(timeOut=10000L, dependsOnMethods="testWaitForService")
+    public void testWaitForService2() throws InterruptedException, ExecutionException {
+        logTest();
+
+        Bundle hostBundle = null;
+        Bundle helloBundle = getBundlesBySymbolicName("org.ancoron.osgi.test.hello-service").iterator().next();
+        for(Bundle b : bundles) {
+            if("org.ancoron.osgi.test.fragments-simple-host".equals(b.getSymbolicName())) {
+                hostBundle = b;
+                break;
+            }
+        }
+        
+        final Bundle client = hostBundle;
+
+        uninstallBundle(helloBundle);
+        installAndStart("org.ancoron.osgi.test:hello-service");
+
+        Future<Object> svc = Executors.newSingleThreadExecutor().submit(new Callable<Object>() {
+
+            @Override
+            public Object call() throws Exception {
+                ServiceTracker st = new ServiceTracker(client.getBundleContext(),
+                        HelloService.class.getName(), null);
+                st.open();
+                try {
+                    return st.waitForService(10000L);
+                } finally {
+                    st.close();
+                }
+            }
+        });
+        
+        // installBundle(MavenHelper.getArtifact("org.ancoron.osgi.test", "hello-service", "1.0.1-SNAPSHOT", "jar"),
+        //         getFramework().getBundleContext());
+        
+        
+        svc.get();
+    }
 }
