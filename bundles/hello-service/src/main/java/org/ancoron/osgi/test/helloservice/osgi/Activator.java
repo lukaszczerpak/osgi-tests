1395e96287622be11a6f76c8d0b6cef705e723f7
ancoron <ancoron.luciferis@gmail.com>
2011-06-04 00:58:12 +0200

    Add simple "hello" test service

---
+++ bundles/hello-service/src/main/java/org/ancoron/osgi/test/helloservice/osgi/Activator.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2011 ancoron.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ancoron.osgi.test.helloservice.osgi;
+
+import org.ancoron.osgi.test.helloservice.HelloService;
+import org.ancoron.osgi.test.helloservice.impl.HelloServiceImpl;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ *
+ * @author ancoron
+ */
+public class Activator implements BundleActivator {
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        context.registerService(HelloService.class.getName(), new HelloServiceImpl(), null);
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        // services get automatically unregistered...
+    }
+}
